01f37832d02f28f51985c99e8b5af5c16c9c0aaa
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-08-16 11:27:56 +0530

    e-commerse

--- project/app/views.py
+++ project/app/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
 
 def login(req):
+    if req.method=='POST':
+        email1 = req.POST['Email']
+        password = req.POST['password']
+        try:
+            data=Register.objects.get(Email=email1 ,password=password)
+            return redirect(userhome) 
+        except:
+            messages.warning(req, "details not available.")
     return render(req,'login.html')
 
 def register(req):
+    if req.method=='POST':
+        email1=req.POST['Email']
+        name2=req.POST['name']
+        phonenumber3=req.POST['phonenumber']
+        location4=req.POST['location']
+        password5=req.POST['password']
+        try:
+            data=Register.objects.create(Email=email1 , name=name2 , phonenumber=phonenumber3 ,location=location4 , password=password5 )
+            data.save()
+            return redirect(login)
+        except:
+            messages.warning(req, "Your account expires in three days.")
     return render(req,'register.html')
 
 def userhome(req):
