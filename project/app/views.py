39e529ae0c8f022c426e68024db6310fd169eddf
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-08-17 22:48:44 +0530

    e-commerse processing

--- project/app/views.py
+++ project/app/views.py
@@ -1,67 +1,91 @@
+
 from django.shortcuts import render,redirect
 from .models import *
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
-
 # Create your views here.
+
+def get_usr(req):
+    data=Register.objects.get(Email=req.session['user'])
+    return data
+
+
+
+
 
 def login(req):
     if 'user' in req.session:
         return redirect(userhome)
 
     if req.method=='POST':
-        email1 = req.POST['Email']
-        password = req.POST['password']
+        email=req.POST['Email']
+        password=req.POST['password']
         try:
-            data=Register.objects.get(Email=email1 ,password=password)
+            data=Register.objects.get(Email=email,password=password)
             req.session['user']=data.Email
-            return redirect(userhome) 
+            return redirect(userhome)
         except:
-            shop=auth.authenticate(username=email1 , password=password)
+            shop=auth.authenticate(username=email,password=password)
             if shop is not None:
                 auth.login(req,shop)
-                req.session['shop']=email1
+                req.session['shop']=email
+
                 return redirect(adminhome)
-            messages.warning(req, "details not available.")
+
+
+            messages.warning(req, "INVALID INPUT !")
     return render(req,'login.html')
+
+
 
 def logout(req):
     if 'user' in req.session:
+        # req.session.flush()
         del req.session['user']
     if 'shop' in req.session:
         del req.session
-        auth.logout()
-    return redirect(login)    
+    return redirect(login)
+
 
 def register(req):
+
     if req.method=='POST':
-        email1=req.POST['Email']
-        name2=req.POST['name']
+        name1=req.POST['name']
+        email2=req.POST['email']
         phonenumber3=req.POST['phonenumber']
         location4=req.POST['location']
         password5=req.POST['password']
         try:
-            data=Register.objects.create(Email=email1 , name=name2 , phonenumber=phonenumber3 ,location=location4 , password=password5 )
+            data=Register.objects.create(name=name1,Email=email2,phonenumber=phonenumber3,location=location4,password=password5)
             data.save()
             return redirect(login)
         except:
-            messages.warning(req, "Your account expires in three days.")
+            messages.warning(req, "Email Already Exits , Try Another Email.")
     return render(req,'register.html')
 
+
 def userhome(req):
-    return render(req,'userhome.html')
-
-def adminhome(req):
-
-    return render(req,'adminhome.html')
-
-def profile(req):
     if 'user' in req.session:
-        data=Register.objects.get(Email=req.session['user'])
-        return render(req,'profile.html',{'data':data})
+        return render(req,'userhome.html')
     else:
         return redirect(login)
 
+
+def adminhome(req):
+    
+    return render(req,'adminhome.html')
+
+
+###profile
+def profile(req):
+    if 'user' in req.session:
+        # data=Register.objects.get(Email=req.session['user'])
+        return render(req,'profile.html',{'data':get_usr(req)})
+    else:
+        return redirect(login)
+    
+
+###profile update
 def upload(req):
     if 'user' in req.session:
         data=Register.objects.get(Email=req.session['user'])
@@ -69,8 +93,25 @@ def upload(req):
             name=req.POST['name']
             phonenumber=req.POST['phonenumber']
             location=req.POST['location']
-            Register.objects.filter(Email=req.session['user']).update(name=name ,phonenumber=phonenumber ,location=location)
+            Register.objects.filter(Email=req.session['user']).update(name=name,phonenumber=phonenumber,location=location)
             return redirect(profile)
         return render(req,'upload.html',{'data':data})
+
     else:
-        return redirect(login)
+       return redirect(login)
+      
+  
+def viewuser(req):
+    return render(req,'viewuser.html')
+
+
+def addpro(req):
+    return render(req,'addpro.html')
+
+
+def viewpro(req):
+    return render(req,'viewpro.html')
+
+
+def bookinghistry(req):
+    return render(req,'bookinghistry.html')
